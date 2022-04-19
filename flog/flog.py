c8f5be2c914db9d91b9b462dae327042f49b8a54
zqqf16 <zqqf16@gmail.com>
2022-04-19 17:26:32 +0800

    Make --output optional

--- flog/flog.py
+++ flog/flog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+from ast import arg
 
 from .rule import *
 from .engine import *
@@ -9,16 +10,24 @@ def cmd():
     parser = argparse.ArgumentParser(description='Process logs.')
     parser.add_argument('-r', '--rule', help='Rules file', required=True)
     parser.add_argument('src', help='Source file', nargs='+')
-    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('-o', '--output', dest='output', help='Output file', nargs='*')
     return parser.parse_args()
 
 def main():
     args = cmd()
+
     rules = Rule.load(args.rule)
     engine = Engine(rules)
 
-    output = args.output or args.src[0] + '.out'
+    output = None
+    if args.output != None:
+        if len(args.output) > 0:
+            output = args.output[0]
+        else:
+            output = args.src[0] + '.out'
+
     for idx, src in enumerate(args.src):
         engine.run(src, output, idx > 0)
-
-    print('Output file:', output)
+    
+    if output:
+        print('Output file:', output)
